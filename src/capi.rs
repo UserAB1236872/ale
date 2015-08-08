c92c6cd421627f6e9dda1978ccf8382eb7307520
Jeff Juozapaitis <Jragonmiris@gmail.com>
2015-08-08 04:04:02 -0700

    Fixed warnings

--- src/capi.rs
+++ src/capi.rs
@@ -3,18 +3,18 @@ extern crate libc;
 use std::ffi::{CStr, CString};
 use std::ops::Drop;
 
-use self::libc::{c_char, c_int, c_float, c_uchar, c_void};
+use self::libc::{c_char, c_int, c_float, c_uchar};
 
 use std::convert::Into;
 
 #[derive(Clone,Copy,Eq,PartialEq,Hash,Debug)]
-pub struct Action(i32);
+pub struct Action(pub i32);
 
 pub struct ALE {
-    p: *mut ale_interface
+    p: *mut AleInterface
 }
 
-enum ale_interface{}
+enum AleInterface{}
 
 impl ALE {
     pub fn new() -> ALE {
@@ -317,51 +317,51 @@ impl Into<ALE> for Game {
 extern {
     // Creation/Deletion functions
 
-    fn ALE_new() -> *mut ale_interface;
-    fn ALE_del(i: *mut ale_interface);
+    fn ALE_new() -> *mut AleInterface;
+    fn ALE_del(i: *mut AleInterface);
 
 
     // Getters
-    fn getString(i: *mut ale_interface, key: *const c_char) -> *const c_char;
-    fn getBool(i: *mut ale_interface, key: *const c_char) -> c_int;
-    fn getInt(i: *mut ale_interface, key: *const c_char) -> c_int;
-    fn getFloat(i: *mut ale_interface, key: *const c_char) -> c_float;
+    fn getString(i: *mut AleInterface, key: *const c_char) -> *const c_char;
+    fn getBool(i: *mut AleInterface, key: *const c_char) -> c_int;
+    fn getInt(i: *mut AleInterface, key: *const c_char) -> c_int;
+    fn getFloat(i: *mut AleInterface, key: *const c_char) -> c_float;
 
     // Setters
-    fn setString(i: *mut ale_interface, key: *const c_char, val: *const c_char);
-    fn setBool(i: *mut ale_interface, key: *const c_char, val: c_int);
-    fn setInt(i: *mut ale_interface, key: *const c_char, val: c_int);
-    fn setFloat(i: *mut ale_interface, key: *const c_char, val: c_float);
+    fn setString(i: *mut AleInterface, key: *const c_char, val: *const c_char);
+    fn setBool(i: *mut AleInterface, key: *const c_char, val: c_int);
+    fn setInt(i: *mut AleInterface, key: *const c_char, val: c_int);
+    fn setFloat(i: *mut AleInterface, key: *const c_char, val: c_float);
 
-    fn loadROM(i: *mut ale_interface, file_name: *const c_char);
+    fn loadROM(i: *mut AleInterface, file_name: *const c_char);
 
     // General emulation
-    fn act(i: *mut ale_interface, action: c_int) -> c_int;
-    fn game_over(i: *mut ale_interface) -> c_int;
-    fn reset_game(i: *mut ale_interface);
+    fn act(i: *mut AleInterface, action: c_int) -> c_int;
+    fn game_over(i: *mut AleInterface) -> c_int;
+    fn reset_game(i: *mut AleInterface);
     
     // Action getters
-    fn getLegalActionSet(i: *mut ale_interface, actions: *mut c_int);
-    fn getLegalActionSize(i: *mut ale_interface) -> c_int;
-    fn getMinimalActionSet(i: *mut ale_interface, actions: *mut c_int);
-    fn getMinimalActionSize(i: *mut ale_interface) -> c_int;
+    fn getLegalActionSet(i: *mut AleInterface, actions: *mut c_int);
+    fn getLegalActionSize(i: *mut AleInterface) -> c_int;
+    fn getMinimalActionSet(i: *mut AleInterface, actions: *mut c_int);
+    fn getMinimalActionSize(i: *mut AleInterface) -> c_int;
 
-    fn getFrameNumber(i: *mut ale_interface) -> c_int;
-    fn lives(i: *mut ale_interface) -> c_int;
-    fn getEpisodeFrameNumber(i: *mut ale_interface) -> c_int;
+    fn getFrameNumber(i: *mut AleInterface) -> c_int;
+    fn lives(i: *mut AleInterface) -> c_int;
+    fn getEpisodeFrameNumber(i: *mut AleInterface) -> c_int;
 
     // Screen functions
-    fn getScreenWidth(i: *mut ale_interface) -> c_int;
-    fn getScreenHeight(i: *mut ale_interface) -> c_int;
-    fn getScreen(i: *mut ale_interface, buf: *const c_uchar);
-    fn getScreenRGB(i: *mut ale_interface, buf: *const c_uchar);
+    fn getScreenWidth(i: *mut AleInterface) -> c_int;
+    fn getScreenHeight(i: *mut AleInterface) -> c_int;
+    fn getScreen(i: *mut AleInterface, buf: *const c_uchar);
+    fn getScreenRGB(i: *mut AleInterface, buf: *const c_uchar);
 
     // RAM
-    fn getRAMSize(i: *mut ale_interface) -> c_int;
-    fn getRAM(i: *mut ale_interface, buf: *const c_uchar);
+    fn getRAMSize(i: *mut AleInterface) -> c_int;
+    fn getRAM(i: *mut AleInterface, buf: *const c_uchar);
 
     // State and screen saving
-    fn saveState(i: *mut ale_interface);
-    fn loadState(i: *mut ale_interface);
-    fn saveScreenPNG(i: *mut ale_interface, file_name: *const c_char);
+    fn saveState(i: *mut AleInterface);
+    fn loadState(i: *mut AleInterface);
+    fn saveScreenPNG(i: *mut AleInterface, file_name: *const c_char);
 }
